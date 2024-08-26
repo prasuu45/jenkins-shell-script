0d249e22ee31341d8fc6bdcb66dfdadb98623dd0
prasuu45 <prasuuchow93@gmail.com>
2024-08-26 15:42:59 +0530

    gi installation

---
+++ git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [USERID -ne o]
+then
+  echo "you need to be root user to excuite this script"
+
+fi
+
+dnf install git -y 
+
+if [$? -ne o]
+then
+   echo "installing git is failure"
+else
+  echo "installing git is success"
+
+fi
