ed7235aa703408f449ee4e3df2dd9378032861a6
prasuu45 <prasuuchow93@gmail.com>
2024-08-26 15:51:31 +0530

    gi installation

--- installation.sh
+++ installation.sh
@@ -7,3 +7,12 @@ then
   echo "you need to be root uesr to excute the script"
 fi 
 
+yum update –y
+if [$? -ne 0]
+then
+  echo "updating yum is faliure"
+else 
+  echo "updating yum is success"
+fi
+
+
