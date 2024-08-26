ef2460e848f1d52d9473bd542f9cdd5b53be00ba
prasuu45 <prasuuchow93@gmail.com>
2024-08-26 16:04:33 +0530

    jenkins installation

--- installation.sh
+++ installation.sh
@@ -16,4 +16,37 @@ else
   echo "updating yum is success"
 fi
 
+wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
+fi [$? -ne 0]
+then
+  echo "adding jenkins repo failure"
+else
+  echo "adding jenkins repo success"
+fi
+
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+fi [$? -ne 0]
+then
+  echo "importing jenkins key is failure"
+else
+  echo "mporting jenkins key is success"
+fi
+
+yum upgrade
+fi [$? -ne 0]
+then
+  echo "update is failure"
+else
+  echo "update is success"
+fi
+
+dnf install java-17-amazon-corretto -y
+
+if [$? -ne 0]
+then 
+  echo "installation java 17 is failure"
+else
+  echo "installation java 17 is success"
+fi
