6e633be2b507faff765350218c9dab3af84c4fb9
prasuu45 <prasuuchow93@gmail.com>
2024-08-27 13:44:08 +0530

    install

--- git.sh
+++ git.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
-if [$USERID -ne o]
+if [ $? -ne 0 ]
 then
-  echo "you need to be root user to excuite this script"
-
+  echo "you need to root user excuit the script"
 fi
 
-dnf install git -y 
-
-if [$? -ne o]
-then
-   echo "installing git is failure"
-else
-  echo "installing git is success"
-
+if [ $? -ne 0 ]
+then 
+  echo "git is not installed ; then install git"
+  dnf install git -y
+  if [ $? -ne 0 ]
+  then 
+    echo "git installation failure ... please check it"
+    exit 1
+  else 
+    echo "git installation success"
+  fi
+else 
+   echo "git is already installed ; nothing to do "
 fi
+
+if [ $? -ne 0 ]
+then 
+  echo "mysql not install ; then install mysql"
+  dnf install mysql -y
+  if [ $? -ne 0 ]
+  then
+    echo "mysql installation is failure .. please check it"
+    exit 1
+  else 
+    echo "mysql installation success"
+  fi
+
+else 
+  echo "mysql already install ; nthing to do"
+fi  
+
