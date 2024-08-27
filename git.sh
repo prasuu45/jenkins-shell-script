2ebce2b4d8b9e960413d70484170c4aa35dd47b8
prasuu45 <prasuuchow93@gmail.com>
2024-08-27 14:27:36 +0530

    install

--- git.sh
+++ git.sh
@@ -1,39 +1,53 @@
-#!/bin/bash
+#!/bn/bash
 
 USERID=$(id -u)
-if [ $? -ne 0 ]
-then
-  echo "you need to root user excuit the script"
-fi
 
-if [ $? -ne 0 ]
-then 
-  echo "git is not installed ; then install git"
-  dnf install git -y
-  if [ $? -ne 0 ]
-  then 
-    echo "git installation failure ... please check it"
-    exit 1
-  else 
-    echo "git installation success"
+CHECK_ROOT(){
+  if [ $USERID -ne o ]
+  then
+       echo "please run this script with root user priveleges"
+       exit 1
   fi
-else 
-   echo "git is already installed ; nothing to do "
-fi
+}
 
-if [ $? -ne 0 ]
-then 
-  echo "mysql not install ; then install mysql"
-  dnf install mysql -y
+VALIDATE(){
   if [ $? -ne 0 ]
   then
-    echo "mysql installation is failure .. please check it"
-    exit 1
-  else 
-    echo "mysql installation success"
+      echo "$2 is ... failure"
+  else
+      echo "$2 is ... success"
   fi
+}
 
-else 
-  echo "mysql already install ; nthing to do"
-fi  
+CHECK_ROOT
+
+dnf install git -y
+if [ $? -ne 0 ]
+then 
+    echo "git is not installed installed . going to install git"
+    dnf install git -y
+    VALIDATE $? "install GIT"
+else
+   echo "git is already installed . nothing to do"
+fi
+
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+  echo "mysql is not installed . going to install mysql"
+  dnf install mysql -y
+  VALIDATE $? "install MYSQL"
+else
+   echo "mysql already installed nothing to do"
+fi
+
+dhf install nginx -y
+if [ $? -ne 0 ]
+then 
+   echo "nginx is not installed . going to install nginx"
+   dnf install nginx -y
+   VALIDATE $? "install NGINX "
+else
+   echo "nginx already installed. nothing to do"
+fi
 
